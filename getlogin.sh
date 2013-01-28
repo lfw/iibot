5b581273143e89933fd6720bfe8148dc37d3f36f
Ian Laird <en0@mail.com>
2013-01-27 20:32:40 -0700

    Adding a user login cache to speed up the username check. This does require users have a login on the irc server.

--- getlogin.sh
+++ getlogin.sh
@@ -11,6 +11,14 @@ SCRIPT_DIR=$(readlink -f $(dirname $0))
 # Make sure we are running and account for new process
 [ ! -f $PIDFILE ] && printf "ERROR: iibot does not apears to be running.\n" && exit 1
 
+[ ! -f $SCRIPT_DIR/login_cache ] && touch $SCRIPT_DIR/login_cache
+cache=$(grep -iw $_NICK $SCRIPT_DIR/login_cache | cut -d":" -f1)
+
+if [[ ! -z "$cache" ]]; then
+    printf "%s\n" "${cache% *}" | tail -n1
+    exit 0
+fi
+
 printf -- "/whois %s\n" "$_NICK" > $FS/$HOST/in
 STAT_DT="$(stat -c %y $FS/$HOST/out | cut -d' ' -f2)"
 
@@ -18,7 +26,11 @@ while [[ "$STAT_DT" == "$(stat -c %y $FS/$HOST/out | cut -d' ' -f2)" ]]; do
     sleep 3
 done
 
-tail -n 10 $FS/$HOST/out | grep -i "is logged in as" |
-while read -r date time nick user garbage; do
-    [[ "$_NICK" == "${nick,,}" ]] && echo "${user,,}"
-done | tail -n1
+V=$(tail -n 10 $FS/$HOST/out | grep -i "is logged in as" | \
+while read -r date time nick user garbage; do \
+    [[ "$_NICK" == "${nick,,}" ]] && \
+    echo ${user,,}; \
+done | tail -n1) 
+
+printf -- "%s:%s\n" "${V}" "${_NICK,,}" >> $SCRIPT_DIR/login_cache
+printf -- "%s\n" " $V"
