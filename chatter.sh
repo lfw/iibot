e33358f6e59cf74fa2cbbf2f944b89fd31c8ad13
Dan Daggett <socialgeek@gmail.com>
2013-02-21 20:19:51 -0700

    Comment out chatter.sh in preparation for Tor.  Timing problems (me thinks) with some of the chatter.

--- chatter.sh
+++ chatter.sh
@@ -2,7 +2,9 @@
 
 # This script executes when a message is recieved from a user that is not prefixed with a '!'.
 # This script is intended to reconize key words and react to them accordingly.
-
+# Everything is commented out by default.  Had some problems over Tor that I suspect is due to 
+# latency where the message would be flooded over and over into the channel, but have not investigated
+# or confirmed this suspicion.
 MYPID=$$
 
 # Make it all lower case to avoid confusion.
@@ -28,47 +30,47 @@ SCRIPT_DIR=$(readlink -f $(dirname $0))
 #    exit
 
 # Respond to java
-[[ "${M_MSG}" =~ " java " ]] || \
-    [[ "${M_MSG}" =~ " java" ]] || \
-    [[ "${M_MSG}" =~ "java " ]] && \
-    printf -- "You know what my favorite Java feature is?\nRemote code execution.\n" && \
-    exit
+#[[ "${M_MSG}" =~ " java " ]] || \
+#    [[ "${M_MSG}" =~ " java" ]] || \
+#    [[ "${M_MSG}" =~ "java " ]] && \
+#    printf -- "You know what my favorite Java feature is?\nRemote code execution.\n" && \
+#    exit
 
 
 # Respond to dream
-[[ "${M_MSG}" =~ " dream " ]] || \
-    [[ "${M_MSG}" =~ " dream" ]] || \
-    [[ "${M_MSG}" =~ "dream " ]] && \
-    printf -- "Some people live to dream, and some people live to crush those dreams!\n" && \
-    exit
+#[[ "${M_MSG}" =~ " dream " ]] || \
+#    [[ "${M_MSG}" =~ " dream" ]] || \
+#    [[ "${M_MSG}" =~ "dream " ]] && \
+#    printf -- "Some people live to dream, and some people live to crush those dreams!\n" && \
+#    exit
 
 # Respond to right
-[[ "${M_MSG}" =~ " right " ]] || \
-    [[ "${M_MSG}" =~ " right" ]] || \
-    [[ "${M_MSG}" =~ "right " ]] && \
-    printf -- "Yep, that's right. You know it!\n" && \
-    exit
+#[[ "${M_MSG}" =~ " right " ]] || \
+#    [[ "${M_MSG}" =~ " right" ]] || \
+#    [[ "${M_MSG}" =~ "right " ]] && \
+#    printf -- "Yep, that's right. You know it!\n" && \
+#    exit
 
 # Respond to marco
-[[ "${M_MSG}" =~ "marco" ]] && \
-    printf -- "Have you seen Polo? I've been searching for him all day.\n" && \
-    exit
+#[[ "${M_MSG}" =~ "marco" ]] && \
+#    printf -- "Have you seen Polo? I've been searching for him all day.\n" && \
+#    exit
 
 # Respond to polo
-[[ "${M_MSG}" =~ "polo" ]] && \
-    printf -- "Have you seen Marco?! He owes me money!\n" && \
-    exit
+#[[ "${M_MSG}" =~ "polo" ]] && \
+#    printf -- "Have you seen Marco?! He owes me money!\n" && \
+#    exit
 
 # Respond to waldo
-[[ "${M_MSG}" =~ "where is waldo" ]] || \
-    [[ "${M_MSG}" =~ "where's waldo" ]] || \
-    [[ "${M_MSG}" =~ "wheres waldo" ]] && \
-    printf -- "Where is Waldo? Why are you asking me?\n" && \
-    exit
+#[[ "${M_MSG}" =~ "where is waldo" ]] || \
+#    [[ "${M_MSG}" =~ "where's waldo" ]] || \
+#    [[ "${M_MSG}" =~ "wheres waldo" ]] && \
+#    printf -- "Where is Waldo? Why are you asking me?\n" && \
+#    exit
 
-[[ "${M_MSG}" =~ "shutup ${NICK,,}" ]] || \
-    [[ "${M_MSG}" =~ "shut up ${NICK,,}" ]] || \
-    [[ "${M_MSG}" =~ "shut-up ${NICK,,}" ]] && \
-    printf -- "Okay Okay, Calm down.\n" && \
-    exit
+#[[ "${M_MSG}" =~ "shutup ${NICK,,}" ]] || \
+#    [[ "${M_MSG}" =~ "shut up ${NICK,,}" ]] || \
+#    [[ "${M_MSG}" =~ "shut-up ${NICK,,}" ]] && \
+#    printf -- "Okay Okay, Calm down.\n" && \
+#    exit
 
